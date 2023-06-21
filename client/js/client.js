f23764852a0da31167f78ed842a61890b4e311f8
justinTsugranes <justinjontsugranes@gmail.com>
2023-06-21 14:36:46 -0400

    add initial checks to js functions

--- client/js/client.js
+++ client/js/client.js
@@ -1,61 +1,11 @@
 // Reference needed DOM elements
-const userIdInput = document.getElementById("user-id");
-const fetchUserBtn = document.getElementById("fetch-user-btn");
-const userIdDisplay = document.getElementById("userId");
-const pointsDisplay = document.getElementById("points");
-const newPointsInput = document.getElementById("new-points");
-const updatePointsBtn = document.getElementById("update-points-btn");
-const errorMessageDisplay = document.getElementById("error-message"); // New
+const userIdInput = document.getElementById('user-id');
+const fetchUserBtn = document.getElementById('fetch-user-btn');
+const pointsInput = document.getElementById('points');
+const newPointsInput = document.getElementById('new-points');
+const updatePointsBtn = document.getElementById('update-points-btn');
 
-// Fetch a single user's data based on their ID
-// Fetch a single user's data based on their ID
-async function fetchUser(id) {
-  try {
-    const response = await fetch(`/users/${id}`)
-    const user = await response.json()
-
-    if (response.ok) {
-      console.log(user)
-      document.querySelector('#user-name').textContent = user.name;
-      document.querySelector('#points').textContent = user.points_balance;
-
-      setErrorMessageVisibility(false); // Hide the error message
-    } else {
-      throw new Error(user.message)
-    }
-  } catch (error) {
-    console.error('An error occurred:', error)
-    setErrorMessageVisibility(true, error.message); // Show the error message
-  }
-}
-
-async function updateUserPoints(id, points) {
-  try {
-    const response = await fetch(`/users/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ points }),
-    })
-    const user = await response.json()
-
-    if (response.ok) {
-      console.log(user)
-      document.querySelector('#user-name').textContent = user.name;
-      document.querySelector('#points').textContent = user.points_balance;
-
-      setErrorMessageVisibility(false); // Hide the error message
-    } else {
-      throw new Error(user.message)
-    }
-  } catch (error) {
-    console.error('An error occurred:', error)
-    setErrorMessageVisibility(true, error.message); // Show the error message
-  }
-}
-
-// helper function
+// Helper function to handle error message visibility
 function setErrorMessageVisibility(show, message = '') {
   const errorElement = document.getElementById('error-message');
 
@@ -64,11 +14,70 @@ function setErrorMessageVisibility(show, message = '') {
     errorElement.style.display = 'block';
   } else {
     errorElement.style.display = 'none';
+    errorElement.textContent = ''; // Empty the error message
+  }
+}
+
+// Fetch a single user's data based on their ID
+async function fetchUser(id) {
+  if (!id) {
+    setErrorMessageVisibility(true, 'User ID cannot be empty');
+    return;
+  }
+
+  try {
+    const response = await fetch(`/users/${id}`);
+    const user = await response.json();
+
+    if (response.ok) {
+      console.log(user);
+      document.querySelector('#userId').textContent = id;
+      pointsInput.textContent = user.points_balance;
+
+      setErrorMessageVisibility(false); // Hide the error message
+    } else {
+      throw new Error(user.message);
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+    setErrorMessageVisibility(true, error.message); // Show the error message
+  }
+}
+
+async function updateUserPoints(id, points) {
+  if (!id || !points) {
+    setErrorMessageVisibility(true, 'User ID and points cannot be empty');
+    return;
+  }
+
+  try {
+    const response = await fetch(`/users/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ points }),
+    });
+
+    const user = await response.json();
+
+    if (response.ok) {
+      console.log(user);
+      document.querySelector('#userId').textContent = id;
+      pointsInput.textContent = user.points_balance;
+
+      setErrorMessageVisibility(false); // Hide the error message
+    } else {
+      throw new Error(user.message);
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+    setErrorMessageVisibility(true, error.message); // Show the error message
   }
 }
 
 // Add event listeners
-fetchUserBtn.addEventListener("click", () => fetchUser(userIdInput.value));
-updatePointsBtn.addEventListener("click", () =>
-  updateUserPoints(userIdInput.value, newPointsInput.value)
+fetchUserBtn.addEventListener('click', () => fetchUser(userIdInput.value.trim()));
+updatePointsBtn.addEventListener('click', () =>
+  updateUserPoints(userIdInput.value.trim(), newPointsInput.value.trim()),
 );
