2e7d95984266f1eb152f28f51919fd2762357a69
justinTsugranes <justinjontsugranes@gmail.com>
2023-06-22 10:22:40 -0400

    update cors on server

--- server/server.js
+++ server/server.js
@@ -1,33 +1,25 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 // Load environment variables from .env file
-dotenv.config()
+dotenv.config();
 
 // Create a new Express application instance
-const app = express()
+const app = express();
 
 // Middleware
-
-const corsOptions = {
-  origin: 'http://fender-rewards.netlify.app',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-// Enable all CORS requests
-app.use(cors(corsOptions))
-// Parse incoming JSON bodies
-app.use(express.json())
+app.use(cors()); // Enable all CORS requests
+app.use(express.json()); // Parse incoming JSON bodies
 
 // Import routes
-const userRoutes = require('./routes/users')
-const transactionRoutes = require('./routes/transactions')
+const userRoutes = require('./routes/users');
+const transactionRoutes = require('./routes/transactions');
 
 // Use routes
-app.use('/user', userRoutes)
-app.use('/transactions', transactionRoutes)
+app.use('/users', userRoutes);
+app.use('/transactions', transactionRoutes);
 
 // Connect to MongoDB
 mongoose
@@ -36,10 +28,10 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log('Connected to MongoDB successfully.'))
-  .catch((err) => console.error('Failed to connect to MongoDB:', err))
+  .catch((err) => console.error('Failed to connect to MongoDB:', err));
 
 // Specify the port
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // Start the server and listen on the specified port
-app.listen(port, () => console.log(`Server is running on port: ${port}`))
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
