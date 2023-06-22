032f4d241b802e688cc9dc32c3d52ec64c215c83
justinTsugranes <justinjontsugranes@gmail.com>
2023-06-22 10:30:33 -0400

    update routes according to new models

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,94 +1,81 @@
-const express = require('express')
-const router = express.Router()
-const Transaction = require('../models/Transaction')
-const User = require('../models/User')
+const express = require('express');
+const router = express.Router();
+const Transaction = require('../models/Transaction');
+const User = require('../models/User');
 
 // Create Transaction
 router.post('/', async (req, res) => {
-  const user = await User.findOne({ id: req.body.user_id })
-  if (!user) return res.status(404).json({ message: 'User not found' })
+  try {
+    const user = await User.findOne({ userId: req.body.userId });
+    if (!user) return res.status(404).json({ message: 'User not found' });
 
-  const points = req.body.points
-  if (user.points_balance + points <= 0) {
-    return res.status(400).json({ message: 'Insufficient points' })
+    const points = req.body.points;
+    if (user.points < points) {
+      return res.status(400).json({ message: 'Insufficient points' });
+    }
+
+    const transaction = new Transaction({
+      user_id: req.body.userId,
+      payer: req.body.payer,
+      points: req.body.points,
+    });
+    const newTransaction = await transaction.save();
+
+    // Deduct points from user's balance
+    user.points -= req.body.points;
+    await user.save();
+
+    res.status(201).json(newTransaction);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-
-  const transaction = new Transaction({
-    user_id: req.body.user_id,
-    payer: req.body.payer,
-    points: req.body.points,
-  })
-  const newTransaction = await transaction.save()
-
-  // Also update user's points_balance
-  user.points_balance += req.body.points
-  await user.save()
-
-  res.status(201).json(newTransaction)
-})
+});
 
 // Spend points
 router.post('/spend', async (req, res) => {
-  // Get user from database
-  const user = await User.findOne({ id: req.body.user_id })
+  try {
+    const user = await User.findOne({ userId: req.body.userId });
+    if (!user) return res.status(404).json({ message: 'User not found' });
 
-  // Check if user exists
-  if (!user) return res.status(404).json({ message: 'User not found' })
-
-  // Get the points to be spent from the request body
-  let pointsToSpend = req.body.points
-
-  // Check if the user has enough points
-  if (user.points_balance < pointsToSpend) {
-    return res.status(400).json({ message: 'Insufficient points' })
-  }
-
-  // Get the user's transactions, sorted by timestamp
-  const transactions = await Transaction.find({
-    user_id: req.body.user_id,
-  }).sort('timestamp')
-
-  // Array to hold the points deducted from each payer
-  let pointsDeducted = []
-
-  // Iterate over the transactions
-  for (let transaction of transactions) {
-    // If the transaction points are less or equal to the points to spend
-    if (transaction.points <= pointsToSpend) {
-      // Deduct the points of the transaction from the points to spend
-      pointsToSpend -= transaction.points
-
-      // Add the deducted points to the corresponding payer
-      pointsDeducted.push({
-        payer: transaction.payer,
-        points: -transaction.points,
-      })
-
-      // Mark the transaction points as zero
-      transaction.points = 0
-    } else {
-      // Deduct the points to spend from the transaction points
-      transaction.points -= pointsToSpend
-
-      // Add the deducted points to the corresponding payer
-      pointsDeducted.push({ payer: transaction.payer, points: -pointsToSpend })
-
-      // The points to spend are zero now
-      pointsToSpend = 0
+    const pointsToSpend = req.body.points;
+    if (user.points < pointsToSpend) {
+      return res.status(400).json({ message: 'Insufficient points' });
     }
 
-    // Save the transaction
-    await transaction.save()
+    const transactions = await Transaction.find({ user_id: req.body.userId }).sort('timestamp');
+    const pointsDeducted = [];
 
-    // If the points to spend are zero, break the loop
-    if (pointsToSpend === 0) break
+    let remainingPointsToSpend = pointsToSpend;
+
+    for (let transaction of transactions) {
+      if (transaction.points <= remainingPointsToSpend) {
+        const deductedPoints = -transaction.points;
+        pointsDeducted.push({ payer: transaction.payer, points: deductedPoints });
+
+        transaction.points = 0;
+        await transaction.save();
+
+        remainingPointsToSpend -= transaction.points;
+      } else {
+        const deductedPoints = -remainingPointsToSpend;
+        pointsDeducted.push({ payer: transaction.payer, points: deductedPoints });
+
+        transaction.points -= remainingPointsToSpend;
+        await transaction.save();
+
+        remainingPointsToSpend = 0;
+      }
+
+      if (remainingPointsToSpend === 0) break;
+    }
+
+    user.points -= pointsToSpend;
+    await user.save();
+
+    res.json(pointsDeducted);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
+});
 
-  // Update the user's points balance
-  user.points_balance -= req.body.points
-  await user.save()
-
-  res.json(pointsDeducted)
-})
-
-module.exports = router
+module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,46 +1,46 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
 
 // Create User
 router.post('/', async (req, res) => {
   try {
     const user = new User({
-      id: req.body.id,
+      userId: req.body.userId,
       name: req.body.name,
-      points_balance: req.body.points_balance,
-    })
-    const newUser = await user.save()
-    res.status(201).json(newUser)
+      points: req.body.points,
+    });
+    const newUser = await user.save();
+    res.status(201).json(newUser);
   } catch (err) {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 // Get User
 router.get('/:id', async (req, res) => {
   try {
-    const user = await User.findOne({ id: req.params.id })
-    if (!user) return res.status(404).json({ message: 'User not found' })
-    res.json(user)
+    const user = await User.findOne({ userId: req.params.id });
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.json(user);
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 // Update User Points Balance
 router.put('/:id', async (req, res) => {
   try {
-    const user = await User.findOne({ id: req.params.id })
-    if (!user) return res.status(404).json({ message: 'User not found' })
+    const user = await User.findOne({ userId: req.params.id });
+    if (!user) return res.status(404).json({ message: 'User not found' });
 
-    user.points_balance = req.body.points_balance
-    const updatedUser = await user.save()
+    user.points = req.body.points;
+    const updatedUser = await user.save();
 
-    res.json(updatedUser)
+    res.json(updatedUser);
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
